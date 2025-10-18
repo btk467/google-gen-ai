9ec0593b853ed575dfcd4b3c0462ccab96a2ec21
Victor Scurtu <btk467@gmail.com>
2025-10-18 11:15:53 -0500

    added UI with styling, webjars, htmx, sse

--- src/main/java/dev/vms/wcb/googlegenai/chat/ChatController.java
+++ src/main/java/dev/vms/wcb/googlegenai/chat/ChatController.java
@@ -1,61 +1,56 @@
 package dev.vms.wcb.googlegenai.chat;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import reactor.core.publisher.Flux;
+import gg.jte.TemplateEngine;
+import gg.jte.output.StringOutput;
 
-@RestController
+@Controller
 public class ChatController {
 
 	private final ChatClient chatClient;
+    private final TemplateEngine jteTemplateEngine;
 
-	public ChatController(ChatClient.Builder builder) {
+	public ChatController(ChatClient.Builder builder, TemplateEngine jteTemplateEngine) {
 		this.chatClient = builder.build();
+        this.jteTemplateEngine = jteTemplateEngine;
 	}
 
 	@GetMapping("/chat")
 	public String chat() {
-		String userPrompt = """
-				Tell me interesting facts about Java 21 and what are the differences from Java 1.8?
-				""";
-		return this.chatClient
-				.prompt()
-				.user(userPrompt)
-				.call()
-				.content();
+		return "chat/index";
 	}
-	
-	@GetMapping(value = "/stream")
-	public Flux<String> stream( @RequestParam String message) {
-		return this.chatClient
-				.prompt()
-				.system("""
-Your name is Victor. You have a dog Ekatarina, big maincoon cat Timofey and lovely wife Natalia.
-You like russian jokes and movies.
-You hate when people lie.
-You are keeping your conversation in plain English.
-You are trying to be exact when answering questions and if you don't know the answer you saying X3 or xren znaet. 
-You, as a respectful, honest assistant with russian roots who has very good sence of humor is helping the user. 
-Respect means - no russian words.
-						""")
-				.user(message)
-				.stream()
-				.content();
-		
+
+    @PostMapping("/chat")
+    public SseEmitter chat(@RequestParam String message) {
+        SseEmitter emitter = new SseEmitter();
+        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+        sseMvcExecutor.execute(() -> {
+            try {
+                String response = this.chatClient.prompt().user(message).call().content();
+                StringOutput output = new StringOutput();
+                jteTemplateEngine.render("chat/message.jte", response, output);
+                emitter.send(SseEmitter.event().name("message").data(output.toString()));
+                emitter.complete();
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
+    }
+
+	@GetMapping(value = "/chat/sse")
+	public SseEmitter sse() throws IOException {
+        SseEmitter emitter = new SseEmitter();
+        return emitter;
 	}
-//    @GetMapping("/ai/generate")
-//    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-//        return Map.of("generation", this.chatClient.call(message));
-//    }
-//
-//    @GetMapping("/ai/generateStream")
-//	public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-//        Prompt prompt = new Prompt(new UserMessage(message));
-//        return this.chatClient.stream(prompt);
-//    }
 }
